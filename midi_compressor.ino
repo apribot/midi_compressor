593c9f5d3538935b8d4c7c7791112077b14d21e2
apribot <apribot@gmail.com>
2019-12-12 22:52:37 -0500

    go it

--- midi_compressor.ino
+++ midi_compressor.ino
@@ -5,7 +5,8 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() 
 {
-  MIDI.begin(MIDI_CHANNEL_OMNI);  
+  MIDI.begin(MIDI_CHANNEL_OMNI); 
+  MIDI.turnThruOff(); 
 }
 
 void loop()
@@ -14,19 +15,19 @@ void loop()
     // get NoteOn event
     // ... there's an enum in midi_Defs.h for this
     // but i'm not smart enough to figure it out
-    if(MIDI.getType() == 0x90) {
+    if(MIDI.getType() == 0x90 && MIDI.getData2() > 0x00) {
       MIDI.send(
         MIDI.getType(),
         MIDI.getData1(),
-        0x7f, // force to max velocity
-        MIDI.getChannel()
+        0x7f,  //force to max velocity
+        0x01   // channel 1
       );
     } else { 
       MIDI.send(
         MIDI.getType(),
         MIDI.getData1(),
         MIDI.getData2(),
-        MIDI.getChannel()
+        0x01   // channel 1
       );
     }    
   }
